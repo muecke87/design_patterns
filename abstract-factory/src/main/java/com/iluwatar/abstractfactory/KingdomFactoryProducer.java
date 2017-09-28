7d24ed9a87be81725c0478c5259563d00238f8ab
sandromuggli <sandro.muggli@gmail.com>
2017-09-28 22:05:09 +0200

    KingdomFactoryProducer optimization 2

--- abstract-factory/src/main/java/com/iluwatar/abstractfactory/KingdomFactoryProducer.java
+++ abstract-factory/src/main/java/com/iluwatar/abstractfactory/KingdomFactoryProducer.java
@@ -6,16 +6,12 @@ import com.iluwatar.abstractfactory.KingdomFactory;
 import com.iluwatar.abstractfactory.OrcKingdomFactory;
 
 public class KingdomFactoryProducer {
-    public KingdomFactoryProducer() {
-    }
-
     public static KingdomFactory getFactory(String type) {
-
         if(type.equalsIgnoreCase("elf")) {
-            return  new ElfKingdomFactory();
+            return new ElfKingdomFactory();
         } else if(type.equalsIgnoreCase("orc")) {
             return new OrcKingdomFactory();
         }
-        return null;
+        return null; // better throw error;)
     }
 }
